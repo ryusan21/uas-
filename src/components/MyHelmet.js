347013e0e0cbc072333356f49735488e61e09fc5
ryusan21 <rizasanneko@gmail.com>
2025-08-02 20:56:29 +0700

    uas

--- src/components/MyHelmet.js
+++ src/components/MyHelmet.js
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet"
 function MyHelmet({ title, description }) {
   return (
     <Helmet>
-      {title ? <title>{title} | Berpesantren</title> : null}
+      {title ? <title>{title} | Dakimakura</title> : null}
       {description ? <meta name="description" content={description} /> : null}
     </Helmet>
   )
